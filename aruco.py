73e2cb7c3681f3f817e4f12e9fbd4cd6d9ae5abb
Sergio Herreros <81583645+goomii17@users.noreply.github.com>
2023-12-03 16:28:19 +0100

    save before pull

--- aruco.py
+++ aruco.py
@@ -1,10 +1,15 @@
 import cv2
+from time import perf_counter, sleep
 
 
 def capture_aruco(aruco_dict):
     aruco_detector = cv2.aruco.ArucoDetector(aruco_dict, cv2.aruco.DetectorParameters())
     cap = cv2.VideoCapture(0)
+
+    FRAME_RATE = 10
+
     while True:
+        start = perf_counter()
         ret, frame = cap.read()
         corners, ids, rejected = aruco_detector.detectMarkers(frame)
         if ids is not None:
@@ -13,6 +18,12 @@ def capture_aruco(aruco_dict):
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
+        end = perf_counter()
+        time_taken = end - start
+        time_to_sleep = 1 / FRAME_RATE - time_taken
+        if time_to_sleep > 0:
+            sleep(time_to_sleep)
 
     cap.release()
     cv2.destroyAllWindows()
